ff33ba80ef0ffef720de8c199d350249a48c7d61
wcgcyx <wcgcyx@gmail.com>
2023-05-22 16:07:15 +1000

    Add simple NFT renting feature

--- contracts/MyERC721.sol
+++ contracts/MyERC721.sol
@@ -1,9 +1,46 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@imtbl/zkevm-contracts/contracts/token/erc721/preset/ImmutableERC721PermissionedMintable.sol";
 
 contract MyERC721 is ImmutableERC721PermissionedMintable {
+    ///     =====   Data structures  =====
+
+    /// @dev State for lease of ERC721
+    struct LeaseState {
+        // Index of this lease state in the leased ids array
+        uint256 index;
+
+        // Lender address
+        address lender;
+        // Accepted ERC20 token address
+        address paymentTokenAddr;
+        // Lease price
+        uint256 leasePrice;
+        // Lease period in seconds
+        uint256 leasePeriod;
+        // Collateral
+        uint256 collateral;
+
+        // Boolean indicating whether lease is active
+        bool active;
+        // Renter address
+        address renter;
+        // Lease start time in unix seconds
+        uint256 startTime;
+    }
+
+    ///     =====   State Variables  =====
+
+    /// @dev Mapping from <lender, tokenId> to the state of a lease
+    mapping(uint256 => LeaseState) public leaseStates;
+
+    /// @dev Array storing all leased Ids
+    uint256[] public leasedIds;
+
+    ///     =====   Constructor  =====
+    /// @dev Construct a rentable ImmutableERC721PermissionedMintable.
     constructor(
         address owner,
         string memory name,
@@ -12,7 +49,6 @@ contract MyERC721 is ImmutableERC721PermissionedMintable {
         string memory contractURI,
         address receiver, 
         uint96 feeNumerator
-
     )
         ImmutableERC721PermissionedMintable(
             owner,
@@ -24,4 +60,147 @@ contract MyERC721 is ImmutableERC721PermissionedMintable {
             feeNumerator
         )
     {}
+
+    ///     =====  External functions  =====
+
+    /// @dev Allows a token owner to list a token for lease or update a current lease.
+    function listOrUpdateLease(
+        uint256 tokenId,
+        address paymentTokenAddr,
+        uint256 leasePrice,
+        uint256 leasePeriod,
+        uint256 collateral
+    ) external {
+        address sender = _msgSender();
+
+        // Make sure the message sender is the current owner of the given token
+        require(sender == super.ownerOf(tokenId), "Only owner is allowed to list");
+
+        // Make sure this contract has operator permission over the given token
+        require(address(this) == super.getApproved(tokenId), "Contract does not have operator permission");
+
+        // Upsert lease states
+        if (leaseStates[tokenId].lender == address(0)) {
+            leaseStates[tokenId].index = leasedIds.length;
+            leasedIds.push(tokenId);
+        } else {
+            // Make sure sender is not sub-leasing
+            require(!leaseStates[tokenId].active, "Sub-leasing is not allowed");
+        }
+        leaseStates[tokenId].lender = sender;
+        leaseStates[tokenId].paymentTokenAddr = paymentTokenAddr;
+        leaseStates[tokenId].leasePrice = leasePrice;
+        leaseStates[tokenId].leasePeriod = leasePeriod;
+        leaseStates[tokenId].collateral = collateral;
+        leaseStates[tokenId].active = false;
+        leaseStates[tokenId].renter = address(0);
+        leaseStates[tokenId].startTime = 0;
+    }
+
+    /// @dev Allows a token owner to unlist a token.
+    function unlistLease(uint256 tokenId) external {
+        address sender = _msgSender();
+
+        // Make sure the message sender is the current owner of the given token
+        require(sender == super.ownerOf(tokenId), "Only owner is allowed to unlist");
+
+        // Make sure the lease is not active
+        require(!leaseStates[tokenId].active, "Cannot unlist an active lease");
+
+        // Delete lease
+        removeLease(tokenId);
+    }
+
+    /// @dev Attempt to lease a token.
+    function leaseToken(uint256 tokenId) external {
+        address renter = _msgSender();
+
+        // Check if renter is the current owner of the nft
+        require(renter != super.ownerOf(tokenId), "Owner is not allowed to lease");
+
+        // Check if lease exists
+        address lender = leaseStates[tokenId].lender;
+        require(lender != address(0), "Token is not listed for lease");
+
+        // Make lease payment
+        IERC20 paymentContract = IERC20(leaseStates[tokenId].paymentTokenAddr);
+        uint256 price = leaseStates[tokenId].leasePrice;
+        if (!paymentContract.transferFrom(renter, lender, price)) {
+            revert("Not enough tokens to pay lease");
+        }
+
+        // Make collateral payment
+        uint256 collateral = leaseStates[tokenId].collateral;
+        if (!paymentContract.transferFrom(renter, address(this), collateral)) {
+            revert("Not enough tokens to pay collateral");
+        }
+
+        // Make NFT ownership transfer
+        super._transfer(lender, renter, tokenId);
+
+        // Update lease state
+        leaseStates[tokenId].active = true;
+        leaseStates[tokenId].renter = renter;
+        leaseStates[tokenId].startTime = block.timestamp;
+    }
+
+    /// @dev Return leased token.
+    function returnToken(uint256 tokenId) external {
+        address renter = _msgSender();
+
+        // Check if renter is the current renter of the nft.
+        require(renter == leaseStates[tokenId].renter, "Only renter can return token");
+
+        // Make NFT ownership transfer.
+        address lender = leaseStates[tokenId].lender;
+        super._transfer(renter, lender, tokenId);
+
+        // Return collateral
+        IERC20 paymentContract = IERC20(leaseStates[tokenId].paymentTokenAddr);
+        uint256 collateral = leaseStates[tokenId].collateral;
+        if (!paymentContract.transfer(renter, collateral)) {
+            // This should never happen.
+            revert("Not enough balance to pay collateral");
+        }
+
+        // Delete lease
+        removeLease(tokenId);
+    }
+
+    /// @dev Claim collateral if token not returned before lease expiry.
+    function claimCollateral(uint256 tokenId) external {
+        address sender = _msgSender();
+
+        // Make sure the message sender is the original lender
+        require(sender == leaseStates[tokenId].lender, "Only lender is allowed to claim");
+
+        // Make sure the lease has expired
+        require(leaseStates[tokenId].startTime + leaseStates[tokenId].leasePeriod <= block.timestamp, "Lease is not expired");
+
+        // Claim collateral
+        IERC20 paymentContract = IERC20(leaseStates[tokenId].paymentTokenAddr);
+        uint256 collateral = leaseStates[tokenId].collateral;
+        if (!paymentContract.transfer(sender, collateral)) {
+            // This should never happen.
+            revert("Not enough balance to pay collateral");
+        }
+
+        // Delete lease
+        removeLease(tokenId);
+    }
+
+    ///     =====  Private functions  =====
+    
+    /// @dev Remove a listing from the storage
+    function removeLease(uint256 tokenId) private {
+        if (leaseStates[tokenId].lender != address(0)) {
+            // Swap this this element with the last element in the array
+            uint256 current = leaseStates[tokenId].index;
+            uint256 last = leasedIds.length - 1;
+            leaseStates[leasedIds[last]].index = current;
+            leasedIds[current] = leasedIds[last];
+            leasedIds.pop();
+            delete leaseStates[tokenId];
+        }
+    }
 }
--- test/MyERC721.test.ts
+++ test/MyERC721.test.ts
@@ -1,9 +1,11 @@
 import { ethers } from "hardhat";
 import { expect } from "chai";
-import { MyERC721 } from "../typechain-types";
+import { MyERC20, MyERC721 } from "../typechain-types";
+import { time } from "@nomicfoundation/hardhat-network-helpers";
 
 describe("MyERC721", function () {
-  let contract: MyERC721;
+  let erc721: MyERC721;
+  let erc20: MyERC20;
 
   beforeEach(async function () {
     // get owner (first account)
@@ -11,7 +13,7 @@ describe("MyERC721", function () {
 
     // deploy MyERC721 contract
     const MyERC721 = await ethers.getContractFactory("MyERC721");
-    contract = await MyERC721.deploy(
+    erc721 = await MyERC721.deploy(
       owner.address, // owner
       "Imaginary Immutable Iguanas", // name
       "III", // symbol
@@ -20,39 +22,197 @@ describe("MyERC721", function () {
       owner.address,
       ethers.BigNumber.from("2000")
     );
-    await contract.deployed();
+    await erc721.deployed();
+
+    // deploy MyERC20 contract
+    const MyERC20 = await ethers.getContractFactory("MyERC20");
+    erc20 = await MyERC20.deploy(
+      ethers.BigNumber.from("1000")
+    );
 
     // grant owner the minter role
-    await contract.grantRole(await contract.MINTER_ROLE(), owner.address);
+    await erc721.grantRole(await erc721.MINTER_ROLE(), owner.address);
   });
 
   it("Should be deployed with the correct arguments", async function () {
-    expect(await contract.name()).to.equal("Imaginary Immutable Iguanas");
-    expect(await contract.symbol()).to.equal("III");
-    expect(await contract.baseURI()).to.equal("https://example-base-uri.com/");
-    expect(await contract.contractURI()).to.equal(
+    expect(await erc721.name()).to.equal("Imaginary Immutable Iguanas");
+    expect(await erc721.symbol()).to.equal("III");
+    expect(await erc721.baseURI()).to.equal("https://example-base-uri.com/");
+    expect(await erc721.contractURI()).to.equal(
       "https://example-contract-uri.com/"
     );
   });
 
   it("Account with minter role should be able to mint multiple NFTs", async function () {
     const [owner, recipient] = await ethers.getSigners();
-    await contract.connect(owner).mint(recipient.address, 5);
-    expect(await contract.balanceOf(recipient.address)).to.equal(5);
-    expect(await contract.ownerOf(1)).to.equal(recipient.address);
-    expect(await contract.ownerOf(2)).to.equal(recipient.address);
-    expect(await contract.ownerOf(3)).to.equal(recipient.address);
-    expect(await contract.ownerOf(4)).to.equal(recipient.address);
-    expect(await contract.ownerOf(5)).to.equal(recipient.address);
+    await erc721.connect(owner).mint(recipient.address, 5);
+    expect(await erc721.balanceOf(recipient.address)).to.equal(5);
+    expect(await erc721.ownerOf(1)).to.equal(recipient.address);
+    expect(await erc721.ownerOf(2)).to.equal(recipient.address);
+    expect(await erc721.ownerOf(3)).to.equal(recipient.address);
+    expect(await erc721.ownerOf(4)).to.equal(recipient.address);
+    expect(await erc721.ownerOf(5)).to.equal(recipient.address);
   });
 
   it("Account without minter role should not be able to mint NFTs", async function () {
     const [_, acc1] = await ethers.getSigners();
-    const minterRole = await contract.MINTER_ROLE();
+    const minterRole = await erc721.MINTER_ROLE();
     await expect(
-      contract.connect(acc1).mint(acc1.address, 1)
+      erc721.connect(acc1).mint(acc1.address, 1)
     ).to.be.revertedWith(
       `AccessControl: account 0x70997970c51812dc3a010c7d01b50e0d17dc79c8 is missing role ${minterRole}`
     );
   });
+
+  it("Only account with NFT with operator approved can list or update lease", async function() {
+    const [owner, lender] = await ethers.getSigners();
+    await erc721.connect(owner).mint(owner.address, 1);
+
+    await expect(
+      erc721.connect(lender).listOrUpdateLease(1, erc20.address, 10, 3600, 100)
+    ).to.be.revertedWith(
+      `Only owner is allowed to list`
+    );
+
+    await erc721.connect(owner).transferFrom(owner.address, lender.address, 1);
+    await expect(
+      erc721.connect(lender).listOrUpdateLease(1, erc20.address, 10, 3600, 100)
+    ).to.be.revertedWith(
+      `Contract does not have operator permission`
+    );
+
+    await erc721.connect(lender).approve(erc721.address, 1);
+    await expect(
+      erc721.connect(lender).listOrUpdateLease(1, erc20.address, 10, 3600, 100)
+    ).to.be.not.reverted;
+    
+    expect((await erc721.leaseStates(1)).lender).to.equal(lender.address);
+    expect((await erc721.leaseStates(1)).paymentTokenAddr).to.equal(erc20.address);
+    expect((await erc721.leaseStates(1)).leasePrice).to.equal(10);
+    expect((await erc721.leaseStates(1)).leasePeriod).to.equal(3600);
+    expect((await erc721.leaseStates(1)).collateral).to.equal(100);
+
+    await expect(
+      erc721.connect(lender).listOrUpdateLease(1, erc20.address, 20, 7200, 200)
+    ).to.be.not.reverted;
+
+    expect((await erc721.leaseStates(1)).leasePrice).to.equal(20);
+    expect((await erc721.leaseStates(1)).leasePeriod).to.equal(7200);
+    expect((await erc721.leaseStates(1)).collateral).to.equal(200);
+  });
+
+  it("Account should be able to rent and return NFT", async function() {
+    const [owner, lender, renter] = await ethers.getSigners();
+    await erc721.connect(owner).mint(lender.address, 1);
+    await erc721.connect(lender).approve(erc721.address, 1);
+    await expect(
+      erc721.connect(lender).listOrUpdateLease(1, erc20.address, 10, 3600, 100)
+    ).to.be.not.reverted;
+
+    await expect(
+      erc721.connect(renter).leaseToken(1)
+    ).to.be.revertedWith(
+      `ERC20: insufficient allowance`
+    );
+
+    await erc20.connect(owner).transfer(renter.address, 110);
+    await erc20.connect(renter).approve(erc721.address, 110);
+    
+    expect(await erc721.ownerOf(1)).to.equal(lender.address);
+    expect(await erc20.balanceOf(renter.address)).to.equal(110);
+    expect(await erc20.balanceOf(lender.address)).to.equal(0);
+    expect(await erc20.balanceOf(erc721.address)).to.equal(0);
+    await expect(
+      erc721.connect(renter).leaseToken(1)
+    ).to.be.not.reverted;
+
+    expect(await erc721.ownerOf(1)).to.equal(renter.address);
+    expect(await erc20.balanceOf(renter.address)).to.equal(0);
+    expect(await erc20.balanceOf(lender.address)).to.equal(10);
+    expect(await erc20.balanceOf(erc721.address)).to.equal(100);
+
+    await expect(
+      erc721.connect(renter).returnToken(1)
+    ).to.be.not.reverted;
+
+    expect(await erc721.ownerOf(1)).to.equal(lender.address);
+    expect(await erc20.balanceOf(renter.address)).to.equal(100);
+    expect(await erc20.balanceOf(lender.address)).to.equal(10);
+    expect(await erc20.balanceOf(erc721.address)).to.equal(0);
+  });
+
+  it("Account cannot rent NFT if it was listed first but transferred later", async function() {
+    const [owner, lender, renter] = await ethers.getSigners();
+    await erc721.connect(owner).mint(lender.address, 1);
+    await erc721.connect(lender).approve(erc721.address, 1);
+    await expect(
+      erc721.connect(lender).listOrUpdateLease(1, erc20.address, 10, 3600, 100)
+    ).to.be.not.reverted;
+    await erc20.connect(owner).transfer(renter.address, 110);
+    await erc20.connect(renter).approve(erc721.address, 110);
+
+    await erc721.connect(lender).transferFrom(lender.address, owner.address, 1)
+    await expect(
+      erc721.connect(renter).leaseToken(1)
+    ).to.be.revertedWith(
+      `ERC721: transfer from incorrect owner`
+    );
+
+    // Renter will be able to lease if new owner list the NFT again
+    await erc721.connect(owner).approve(erc721.address, 1);
+    await expect(
+      erc721.connect(owner).listOrUpdateLease(1, erc20.address, 10, 3600, 100)
+    ).to.be.not.reverted;
+
+    await expect(
+      erc721.connect(renter).leaseToken(1)
+    ).to.be.not.reverted;
+  })
+
+  it("Account cannot sub lease NFT", async function() {
+    const [owner, lender, renter] = await ethers.getSigners();
+    await erc721.connect(owner).mint(lender.address, 1);
+    await erc721.connect(lender).approve(erc721.address, 1);
+    await expect(
+      erc721.connect(lender).listOrUpdateLease(1, erc20.address, 10, 3600, 100)
+    ).to.be.not.reverted;
+    await erc20.connect(owner).transfer(renter.address, 110);
+    await erc20.connect(renter).approve(erc721.address, 110);
+    await expect(
+      erc721.connect(renter).leaseToken(1)
+    ).to.be.not.reverted;
+
+    await erc721.connect(renter).approve(erc721.address, 1);
+    await expect(
+      erc721.connect(renter).listOrUpdateLease(1, erc20.address, 20, 1800, 100)
+    ).to.be.revertedWith(
+      `Sub-leasing is not allowed`
+    );
+  })
+
+  it("Account only claim collateral when lease is expired", async function() {
+    const [owner, lender, renter] = await ethers.getSigners();
+    await erc721.connect(owner).mint(lender.address, 1);
+    await erc721.connect(lender).approve(erc721.address, 1);
+    await expect(
+      erc721.connect(lender).listOrUpdateLease(1, erc20.address, 10, 3600, 100)
+    ).to.be.not.reverted;
+    await erc20.connect(owner).transfer(renter.address, 110);
+    await erc20.connect(renter).approve(erc721.address, 110);
+    await expect(
+      erc721.connect(renter).leaseToken(1)
+    ).to.be.not.reverted;
+
+    await expect(
+      erc721.connect(lender).claimCollateral(1)
+    ).to.be.revertedWith(
+      `Lease is not expired`
+    );
+
+    await time.increase(3601)
+    await expect(
+      erc721.connect(lender).claimCollateral(1)
+    ).to.be.not.reverted;
+    expect(await erc20.balanceOf(lender.address)).to.equal(110);
+  })
 });
